f0b2cc1aa60c87a8846affee2174e1d02333cadf
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-06-21 16:33:27 -0400

    remove screens managing in main window

--- USFMConverter/UI/MainWindow.axaml.cs
+++ USFMConverter/UI/MainWindow.axaml.cs
@@ -17,7 +17,7 @@ namespace USFMConverter
         public MainWindow()
         {
             InitializeComponent();
-            SetCurrentScreen("ProjectDetailScreen");
+            SetCurrentScreen(nameof(ProjectDetailScreen));
 #if DEBUG
             this.AttachDevTools();
 #endif
@@ -29,36 +29,12 @@ namespace USFMConverter
                 i.Value.IsVisible = i.Key == screen;
             }
         }
-        public void HomeScreen_FolderSelected(object sender, RoutedEventArgs e)
-        {
-            HomeScreen HomeScreen = (HomeScreen)sender;
-            ProjectDetailScreen.Items = LoadFolder(HomeScreen.Folder);
-            SetCurrentScreen("ProjectDetailScreen");
-        }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-            // this.HomeScreen = this.FindControl<HomeScreen>("HomeScreen");
             this.ProjectDetailScreen = this.FindControl<ProjectDetailScreen>("ProjectDetailScreen");
-            // this.Screens.Add("HomeScreen", this.HomeScreen);
-            this.Screens.Add("ProjectDetailScreen", this.ProjectDetailScreen);
-        }
-
-        private List<string> LoadFolder(string folderName)
-        {
-            List<string> output = new();
-            List<string> supportedExtensions = new List<string> { ".usfm", ".txt", ".sfm" };
-            var dirinfo = new DirectoryInfo(folderName);
-            var allFiles = dirinfo.GetFiles("*", SearchOption.AllDirectories);
-            foreach (FileInfo fileInfo in allFiles)
-            {
-                if (supportedExtensions.Contains(Path.GetExtension(fileInfo.FullName.ToLower())))
-                {
-                    output.Add(fileInfo.FullName);
-                }
-            }
-            return output;
+            this.Screens.Add(nameof(ProjectDetailScreen), this.ProjectDetailScreen);
         }
     }
 }
