d4d40430137bd487b332ffc866e4e34d22aed16d
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-06-21 09:56:07 -0400

    drag and drop handler

--- USFMConverter/UI/Pages/ProjectDetailScreen.axaml.cs
+++ USFMConverter/UI/Pages/ProjectDetailScreen.axaml.cs
@@ -1,14 +1,20 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace USFMConverter.UI.Pages
 {
 
     public partial class ProjectDetailScreen : UserControl
     {
+        private Border dragDropArea;
+        private ListBox filesContainer;
+
         private static readonly StyledProperty<List<string>> ItemsProperty = AvaloniaProperty.Register<ProjectDetailScreen, List<string>>(nameof(Items));
         public List<string> Items
         {
@@ -23,9 +29,40 @@ namespace USFMConverter.UI.Pages
         {
             InitializeComponent();
         }
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            Items = new List<string>();
+            filesContainer = this.Find<ListBox>("FilesListBox");
+
+            dragDropArea = this.Find<Border>("DragDropArea");
+            dragDropArea.AddHandler(DragDrop.DragOverEvent, OnDragOver);
+            dragDropArea.AddHandler(DragDrop.DropEvent, OnDrop);
         }
+
+        private void OnDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                e.DragEffects = DragDropEffects.Copy;
+            }
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                var newList = Items.Concat(e.Data.GetFileNames()).ToList();
+                Items = newList;
+
+                //var viewData = (ViewData)DataContext;
+                //viewData.Files = this.Items;
+
+                filesContainer.Items = newList;
+            }
+        }
+
     }
 }
