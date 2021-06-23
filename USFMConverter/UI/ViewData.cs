00af7665c4b271fa57df5b5388c59c00961550fa
Tony Anh Tran <34975907+AnonymousWalker@users.noreply.github.com>
2021-06-23 16:37:36 -0400

    ViewData for UI Binding (#6)

--- USFMConverter/UI/ViewData.cs
+++ USFMConverter/UI/ViewData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,21 @@ namespace USFMConverter.UI
 {
     public class ViewData
     {
-        public List<string> Files { get; set; } = new() {"default.usfm"};
-        public int FontSize { get; set; } = 12;
-        public double LineSpacing { get; set; } = 1.0;
-        public bool LeftToRight { get; set; } = true;
+        public List<string> Files { get; set; } = new();
+
+        public ComboBoxItem OutputFileFormat { get; set; }
+
+        public ComboBoxItem TextSize { get; set; }
+
+        public ComboBoxItem LineSpacing { get; set; }
+
         public int ColumnCount { get; set; } = 1;
+
+        public bool Justified { get; set; } = false;
+        public bool LeftToRight { get; set; } = true;
         public bool ChapterBreak { get; set; } = false;
         public bool VerseBreak { get; set; } = false;
         public bool NoteTaking { get; set; } = false;
         public bool TableOfContents { get; set; } = false;
-        public string OutputLocation { get; set; } = "";
-        public TextAlignment TextAlignment { get; set; } = TextAlignment.LEFT;
     }
 }
