7489f31129a01b740f7aabce712c30384086adfa
Tony Anh Tran <34975907+AnonymousWalker@users.noreply.github.com>
2021-06-24 10:55:56 -0400

    Converter Core (#8)

---
+++ USFMConverter/Core/Render/RenderDocument.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using USFMConverter.Core.Data;
+using USFMToolsSharp;
+using USFMToolsSharp.Models.Markers;
+
+namespace USFMConverter.Core.Render
+{
+    public abstract class RenderDocument
+    {
+        private Action<double> UpdateProgress;
+
+        public RenderDocument()
+        {
+            UpdateProgress = new Action<double>((value) => { return; });
+        }
+
+        public RenderDocument(Action<double> updateProgress)
+        {
+            UpdateProgress = updateProgress;
+        }
+
+        protected USFMDocument LoadUSFM(IEnumerable<FileInfo> files)
+        {
+            var usfmDoc = new USFMDocument();
+            List<FileInfo> fileList = files.ToList();
+
+            var parser = new USFMParser(new List<string> { "s5" });
+            int totalFiles = fileList.Count;
+
+            for (int i = 0; i < totalFiles; i++)
+            {
+                var text = File.ReadAllText(fileList[i].FullName);
+
+                usfmDoc.Insert(parser.ParseFromString(text));
+                // update progress bar
+                var percent = (double)i / totalFiles * 100;
+                UpdateProgress(percent);
+            }
+
+            return usfmDoc;
+        }
+
+        public void Render(Project project)
+        {
+            Render(project, LoadUSFM(project.Files));
+        }
+
+        public abstract void Render(Project project, USFMDocument usfmDoc);
+    }
+}
---
+++ USFMConverter/Core/Render/RenderDocx.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using USFMConverter.Core.Data;
+using USFMToolsSharp.Models.Markers;
+using USFMToolsSharp.Renderers.Docx;
+
+namespace USFMConverter.Core.Render
+{
+    public class RenderDocx : RenderDocument
+    {
+        public RenderDocx()
+        {
+
+        }
+
+        private DocxConfig BuildDocxConfig(RenderFormat format)
+        {
+            return new();
+        }
+
+        public override void Render(Project project, USFMDocument usfm)
+        {
+            var config = BuildDocxConfig(project.FormatOptions);
+            var renderer = new DocxRenderer(config);
+            var document = renderer.Render(usfm);
+
+            using (Stream outputStream = File.Create(project.OutputFile.FullName))
+            {
+                document.Write(outputStream);
+            }
+        }
+    }
+}
