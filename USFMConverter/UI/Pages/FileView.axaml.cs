21ac4eabd89cd80d8c0330222074231609b90e1d
Eric Sangyun Ko <37914691+Thundelly@users.noreply.github.com>
2021-06-29 08:42:25 -0700

    ListBox Height Adjustment

--- USFMConverter/UI/Pages/FileView.axaml.cs
+++ USFMConverter/UI/Pages/FileView.axaml.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Avalonia.Interactivity;
+using NPOI.OpenXmlFormats.Vml.Office;
 using USFMConverter.Core;
 using USFMConverter.Core.Util;
 using USFMConverter.Core.ConstantValue;
