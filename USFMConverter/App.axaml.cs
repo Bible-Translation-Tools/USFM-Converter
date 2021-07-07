faf756c18f31a518713a466f61c9de6b0b94de10
Eric Ko <thundelly0609@gmail.com>
2021-07-07 09:22:18 -0700

    Check and load config before loading mainscreen

--- USFMConverter/App.axaml.cs
+++ USFMConverter/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using USFMConverter.Core.Data;
+using USFMConverter.Core.Util;
 using USFMConverter.UI;
 
 namespace USFMConverter
@@ -16,10 +18,35 @@ namespace USFMConverter
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
+                Setting? setting = FileSystem.LoadConfig();
+
+                // If there exist config files, load them and assign to the data context
+                if (setting != null)
                 {
-                    DataContext = new ViewData()
-                };
+                    desktop.MainWindow = new MainWindow
+                    {
+                        DataContext = new ViewData()
+                        {
+                            SelectedTextSizeIndex = setting.TextSize,
+                            SelectedLineSpacingIndex = setting.LineSpacing,
+                            ColumnCount = setting.ColumnCount,
+                            Justified = setting.Justified,
+                            LeftToRight = setting.LeftToRight,
+                            ChapterBreak = setting.ChapterBreak,
+                            VerseBreak = setting.VerseBreak,
+                            NoteTaking = setting.NoteTaking,
+                            TableOfContents = setting.TableOfContents
+                        }
+                    };
+                }
+                // If the config file does not exist
+                else
+                {
+                    desktop.MainWindow = new MainWindow
+                    {
+                        DataContext = new ViewData()
+                    };
+                }
             }
 
             base.OnFrameworkInitializationCompleted();
