f42dce19b9576831fd404fd5ceedfd51eac49be3
danparisd <43418833+danparisd@users.noreply.github.com>
2021-08-03 08:09:05 -0400

    Create dotnet.yml (#55)
    auto builds

---
+++ .github/scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export INSTALL4J_LICENSE
+
+dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-win -r win-x64
+dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-nix -r linux-x64
+dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-mac -r osx-x64
+
+docker run -v $(pwd):/repo -e INSTALL4J_LICENSE wycliffeassociates/install4j-docker:9.0.2 /install4j/install4j9.0.2/bin/install4jc --license=$INSTALL4J_LICENSE /repo/usfmconverter.install4j
