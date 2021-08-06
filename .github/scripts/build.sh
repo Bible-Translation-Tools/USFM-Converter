eb1fe5c0664c605f284989ad62b86c96590fff69
danparisd <you@example.com>
2021-08-06 18:06:47 -0400

    add mac .app build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,6 +4,8 @@ export INSTALL4J_LICENSE
 
 dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-win -r win-x64
 dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-nix -r linux-x64
-dotnet publish ./USFMConverter/USFMConverter.sln -c Release --self-contained true -o ./output-mac -r osx-x64
+dotnet msbuild ./USFMConverter/USFMConverter.sln -t:BundleApp -p:RuntimeIdentifier=osx-x64 -property:Configuration=Release -p:OutputPath=../output-mac/
 
 docker run -v $(pwd):/repo -e INSTALL4J_LICENSE wycliffeassociates/install4j-docker:9.0.2 /install4j/install4j9.0.2/bin/install4jc --license=$INSTALL4J_LICENSE /repo/usfmconverter.install4j
+
+cp ./output-mac/publish/USFMConverter.app ./installers/USFMConverter.app
