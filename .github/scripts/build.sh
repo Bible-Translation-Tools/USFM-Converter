3cecc45fc8874e73f873dcb1013b841646485835
danparisd <you@example.com>
2021-08-06 18:16:38 -0400

    fix cp

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,4 +8,4 @@ dotnet msbuild ./USFMConverter/USFMConverter.sln -t:BundleApp -p:RuntimeIdentifi
 
 docker run -v $(pwd):/repo -e INSTALL4J_LICENSE wycliffeassociates/install4j-docker:9.0.2 /install4j/install4j9.0.2/bin/install4jc --license=$INSTALL4J_LICENSE /repo/usfmconverter.install4j
 
-cp ./output-mac/publish/USFMConverter.app ./installers/USFMConverter.app
+cp -r ./output-mac/publish/USFMConverter.app ./installers/USFMConverter.app
