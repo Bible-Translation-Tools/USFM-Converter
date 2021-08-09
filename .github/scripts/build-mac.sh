6fbecf646c9bacde2fc0b29bd1ced5e8f0a1a2c7
danparisd <you@example.com>
2021-08-09 18:19:27 -0400

    sln

--- .github/scripts/build-mac.sh
+++ .github/scripts/build-mac.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace
 
-dotnet restore -r osx-x64
+dotnet restore -r osx-x64 ./USFMConverter/USFMConverter.sln
 dotnet msbuild ./USFMConverter/USFMConverter.sln -t:BundleApp -p:RuntimeIdentifier=osx-x64 -property:Configuration=Release -p:OutputPath=../output-mac/
 mkdir ./dmg-source
 cp -r ./output-mac/publish/USFMConverter.app ./dmg-source/USFMConverter.app
